d2802100fe38a59332eef82a519722876d202b8d
drexar1111 <drexar1999@gmail.com>
2020-05-07 22:59:50 +0300

    project pertama

---
+++ 18MY03019 Siber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _18MY03019_Siber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xDocument = XDocument.Load("yaz.xml");
+            XName X = XName.Get("host", xDocument.Root.Name.NamespaceName);
+            foreach (XElement data in xDocument.Descendants(X))
+            {
+                Console.WriteLine(data.ToString());
+                xDocument.Save("metehan.xml");
+            }
+            Console.ReadKey();
+        }
+    }
+}
